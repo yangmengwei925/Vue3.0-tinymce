75ae65592d19df36ae25d9b6cab4652cf531fd55
杨梦伟 <yang1635433207@163.com>
2023-12-15 17:51:49 +0800

    优化代码

---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <!-- <HelloWorld msg=""/> -->
+    <myEditor v-model="value" placeholder="我是placeholder"></myEditor>
+    <br />
+    <button @click="getValue">获取文本内容</button>
+    <br />
+    <div v-html="value"></div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { reactive, toRefs, onMounted } from 'vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import myEditor from '@/components/myEditor'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld,
+    myEditor
+  },
+  setup(props) {
+    const state = reactive({
+      value: ''
+    })
+
+    onMounted(() => {
+      setTimeout(() => {
+        state.value = '99999999999'
+      }, 500);
+    })
+
+    function getValue () {
+      console.log(state.value)
+
+    }
+
+    return {
+      ...toRefs(state),
+      getValue
+    }
+  }
+}
+</script>
